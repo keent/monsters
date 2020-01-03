038b04efd5b55725a59343c706105cb4d2f33a9b
6r4n <granvillelintao@gmail.com>
2020-01-03 06:33:29 +0400

    Updates

---
+++ precache-manifest.7e45b9446fc4d44129c25a221c95dbb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6b7f49cfb05de59ea14d26a35a2057",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "7f34cd1f7f99556e25cd",
+    "url": "/monsters/static/css/main.c846e656.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/monsters/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f34cd1f7f99556e25cd",
+    "url": "/monsters/static/js/main.641e17ef.chunk.js"
+  },
+  {
+    "revision": "bd31af7237f1bbc8dae4",
+    "url": "/monsters/static/js/runtime-main.30c4b08d.js"
+  }
+]);
